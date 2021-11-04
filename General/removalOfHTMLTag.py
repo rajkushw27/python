fc18353e0e0b85917278486c5be215e9dd203667
Rajani Kushwah <rajkushw@Rajanis-MacBook-Air.local>
2021-11-04 11:50:55 +0530

    html tag removal

---
+++ General/removalOfHTMLTag.py
@@ -0,0 +1,18 @@
+def remove_html_markup(s):
+    tag = False
+    quote = False
+    out = ""
+
+    for c in s:
+        if c == '<' and not quote:
+            tag = True
+        elif c == '>' and not quote:
+            tag = False
+        elif c == '"' or c == "'" and tag:
+            quote = not quote
+        elif not tag:
+            out = out + c
+    return out
+
+
+print(remove_html_markup("<b> hello world </b>"))
